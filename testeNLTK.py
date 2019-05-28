81471f5718d02b8ddcb59013a1c0e13a0db431c7
MatheusFeijoo <matheuspuime@gmail.com>
2019-05-28 08:44:07 -0300

    tirando pontuação e stopwords em português

--- testeNLTK.py
+++ testeNLTK.py
@@ -1,22 +1,38 @@
 import nltk
 import csv
+from nltk.corpus import stopwords
 #import pandas as pd
 #precisa baixar os pacotes antes de rodar
 #nltk.download()
 
 
+
 frase = "Fala ai cria, tudo na tranquilidade? O menor da rua de baixo tá chamando a gente pro role, pega logo a peita que tá aqui em casa!!!"
 
-#Gera os tokens da frase
-token_frase = nltk.word_tokenize(frase)
+def pegaGirias():
+    with open('arquivos/girias.csv', 'r', encoding="utf-8") as csvGirias:
+        csv_lendo = list(csv.reader(csvGirias))
+    csvGirias.close()
+    return csv_lendo
 
-#coloca tudo em minusculo
-frase_low = [token_frase.lower() for token_frase in token_frase]
-#print(frase_low)
-array_length = len(frase_low)
+def tokenFrase(frase):
+    #Gera os tokens da frase
+    token_frase = nltk.word_tokenize(frase)
+    #coloca tudo em minusculo
+    palavras = [token_frase.lower() for token_frase in token_frase]
+    #print(frase_low)
+    
+    #table = str.maketrans('', '', string.punctuation)
+    #stripped = [w.translate(table) for w in palavras]
+    words = [word for word in palavras if word.isalpha()]
+    stop_words = set(stopwords.words('portuguese'))
+    words = [w for w in words if not w in stop_words]
+    return words
+
 
 "precisa retirar a pontuação para diminuir as interações"
-    
+
+
 #for i in range(array_length):
 #    print (token_frase[i])
     
@@ -25,26 +41,21 @@ array_length = len(frase_low)
 #print(index,word)
     
 #abre o arquivo csv e coleta as girias 
-with open('arquivos/girias.csv', 'r', encoding="utf-8") as csvGirias:
-    csv_lendo = list(csv.reader(csvGirias))
-csvGirias.close()
 
+fraseRecebida = tokenFrase(frase)
+fraseTamanho = len(fraseRecebida)
 
-csv_length = len(csv_lendo)
+csvGirias = pegaGirias()
+csv_length = len(csvGirias)
 
 for a in range(csv_length):
-    print(csv_lendo[a][0])
+    print(csvGirias[a][0])
 print("------")         
 print(frase)
-for i in range(array_length):
+for i in range(fraseTamanho):
     for j in range(csv_length):
-        if(csv_lendo[j][0] in frase_low[i]):
+        if(csvGirias[j][0] in fraseRecebida[i]):
             print("------")   
-            print("Nesta frase é possível que a palavra "+ csv_lendo[j][0] + " seja uma gíria!")
+            print("Nesta frase é possível que a palavra "+ csvGirias[j][0] + " seja uma gíria!")
             print("Seu significado é: ")
-            print(csv_lendo[j][1])
-
-
-    
-
-
+            print(csvGirias[j][1])
--- teste_csv.py
+++ teste_csv.py
@@ -1,18 +1,9 @@
-#!/usr/bin/python
-# -*- coding: UTF-8 -*-
+# -*- coding: utf-8 -*-
+"""
+Created on Mon May 27 14:28:48 2019
 
-#Abrindo CSV
-import csv
-import pandas as pd
+@author: Matheus
+"""
 
-with open('arquivos/girias.csv', 'r', encoding="utf-8") as csvGirias:
-    reader1 = csv.reader(csvGirias)
-    read = []
-    for row in reader1:
-        if len(row) != 0:
-            read = read + [row]
-            
-csvGirias.close()
-df = pd.DataFrame(read)
-df.rename(columns={'0':'giria','1':'significado','2':'sinonimos','3':'nome','4':'lugar','5':'idade'})
-print(df[0])
+
+
