ae85fb9f4418b655b92ae180ae1731d6fba29543
MatheusFeijoo <matheuspuime@gmail.com>
2019-05-30 10:52:08 -0300

    Criação do chat e conectando com análise de gírias

--- testeNLTK.py
+++ testeNLTK.py
@@ -1,61 +1,51 @@
 import nltk
 import csv
 from nltk.corpus import stopwords
-#import pandas as pd
-#precisa baixar os pacotes antes de rodar
-#nltk.download()
 
-
-
-frase = "Fala ai cria, tudo na tranquilidade? O menor da rua de baixo tá chamando a gente pro role, pega logo a peita que tá aqui em casa!!!"
-
+# Pega gírias do arquivo .csv
+####### Ainda falta opção para inserir novas gírias #######
 def pegaGirias():
     with open('arquivos/girias.csv', 'r', encoding="utf-8") as csvGirias:
         csv_lendo = list(csv.reader(csvGirias))
     csvGirias.close()
     return csv_lendo
 
-def tokenFrase(frase):
-    #Gera os tokens da frase
-    token_frase = nltk.word_tokenize(frase)
-    #coloca tudo em minusculo
+# Parte principal
+def tokenFrase(message):
+
+    # Gera os tokens da frase enviada
+    token_frase = nltk.word_tokenize(message)
+    # Coloca tudo em minusculo
     palavras = [token_frase.lower() for token_frase in token_frase]
-    #print(frase_low)
     
+    #####
+    #  
+    #   Ainda falta tirar a pontuação
+    #
+    #####
     #table = str.maketrans('', '', string.punctuation)
     #stripped = [w.translate(table) for w in palavras]
+    
+    # Tira stopwords
     words = [word for word in palavras if word.isalpha()]
     stop_words = set(stopwords.words('portuguese'))
     words = [w for w in words if not w in stop_words]
-    return words
-
-
-"precisa retirar a pontuação para diminuir as interações"
-
-
-#for i in range(array_length):
-#    print (token_frase[i])
+    fraseTamanho = len(words)
     
-#outra forma de separarar   
-#for index, word in enumerate(nltk.word_tokenize(texto)):
-#print(index,word)
-    
-#abre o arquivo csv e coleta as girias 
-
-fraseRecebida = tokenFrase(frase)
-fraseTamanho = len(fraseRecebida)
-
-csvGirias = pegaGirias()
-csv_length = len(csvGirias)
-
-for a in range(csv_length):
-    print(csvGirias[a][0])
-print("------")         
-print(frase)
-for i in range(fraseTamanho):
-    for j in range(csv_length):
-        if(csvGirias[j][0] in fraseRecebida[i]):
-            print("------")   
-            print("Nesta frase é possível que a palavra "+ csvGirias[j][0] + " seja uma gíria!")
-            print("Seu significado é: ")
-            print(csvGirias[j][1])
+    # Chama o arquivo de gírias
+    csvGirias = pegaGirias()
+    csv_length = len(csvGirias)
+    #####
+    #  
+    #   Ainda falta comparar com o dicionário em português
+    #
+    #####
+    tempo = ' '
+    for i in range(fraseTamanho):
+        for j in range(csv_length):
+            if(csvGirias[j][0] in words[i]):
+                fraseTemp = "\nNesta frase é possível que a palavra "+ csvGirias[j][0] + " seja uma gíria! \n Seu significado é: \n"+ (csvGirias[j][1])
+                tempo = tempo + fraseTemp
+                
+    temp = '\n' + message + tempo
+    return temp
--- teste_csv.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Mon May 27 14:28:48 2019
-
-@author: Matheus
-"""
-
-
-
